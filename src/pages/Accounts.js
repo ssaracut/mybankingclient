d0b95c146557dbed552c1b4352a152e13bc08f08
Bobby Nannier <bobby.nannier@scotiabank.com>
2016-12-29 10:56:18 -0500

    card styles changed

--- src/pages/Accounts.js
+++ src/pages/Accounts.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 
 import { Card, CardActions, CardMenu, CardTitle, CardText } from 'react-mdl/lib/Card'
-import { Button, Cell, Grid, IconButton } from 'react-mdl/lib';
+import { Cell, Grid, IconButton, Spinner } from 'react-mdl/lib';
 
 import AccountsActions from '../core/redux/accounts/AccountsActions'
 
@@ -17,12 +17,7 @@ class AccountsPage extends React.Component {
     render() {
         if (this.props.accounts === null) {
             return (
-                <Card style={{ margin: "0 auto", marginTop: "30px", width: '80%' }}>
-                    <CardTitle>Accounts</CardTitle>
-                    <CardText>
-                        <img src="images/spinner-small.gif" alt="" />
-                    </CardText>
-                </Card>
+                <Spinner style={{margin: 'auto'}} />
             );
         }
         else {
@@ -49,14 +44,15 @@ const renderCell = function(account) {
     return (
         <Cell col={4} key={account.accountKey}>
             <Card shadow={0} style={{ margin: "0 auto", marginBottom: "10px", width: '100%' }}>
-                <CardTitle>{account.name}</CardTitle>
                 <CardText>
-                    {account.description}
+                    <b>{account.name}</b>
+                    <br />{account.number}
                     <br /><br />
-                    <b>$ {account.balance}</b>
+                    <IconButton name="account_balance" /><b>$ {account.balance}</b>
+                    <br /><br /><br />
                 </CardText>
                 <CardActions border>
-                    <Button colored>More quick option icons here</Button>
+                    <IconButton name="more_vert" />
                 </CardActions>
                 <CardMenu style={{color: '#000'}}>
                     <IconButton name="more_vert" />
