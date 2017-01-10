135240b43d536f634a823a3a8e05462cef9d9038
Bobby Nannier <bobby.nannier@scotiabank.com>
2017-01-10 11:13:59 -0500

    Account transaction added back in.

--- src/pages/AccountTransactions.js
+++ src/pages/AccountTransactions.js
@@ -1,29 +1,71 @@
 import React from 'react'
-import { connect } from 'react-redux'
-import { bindActionCreators } from 'redux'
-
-import { Card, CardActions, CardMenu, CardTitle, CardText } from 'react-mdl/lib/Card'
-import { Button, Cell, Grid, IconButton, Spinner } from 'react-mdl/lib';
-
-import AccountsActions from '../core/redux/accounts/AccountsActions'
+import { Button } from 'react-mdl/lib'
+import { Dialog, DialogContent, DialogActions, List, ListItem } from 'react-mdl/lib'
+import './accountTransactions.css';
 
 class AccountTransactions extends React.Component {
-    componentWillMount() {
-        if (this.props.account === null) {
-            //   this.props.accountsActions.getAccounts();
-        }
-    }
-
     render() {
-        if (this.props.account === null) {
-            return (
-                <Spinner style={{margin: 'auto'}}/>
-            );
-        }
-        else {
-            return (
-                <div>Transactions for a single account goes here.</div>
-            );
-        }
+        return (
+            <Dialog open={this.props.accountState.openDialog} onCancel={this.props.handleCloseDialog} className="accountTransactionsDialog">
+                <DialogContent>
+                    <div className="accountTransactionsDialogContentDiv">{renderTransactionsList(this.props.accountState.accountTransactions)}</div>
+                </DialogContent>
+                <DialogActions>
+                    <Button type='button' onClick={this.props.handleCloseDialog}>Close</Button>
+                </DialogActions>
+            </Dialog>
+        );
     }
 }
+
+const renderTransactionsList = function(accountTransactions) {
+    var list = [];
+
+    if (accountTransactions) {
+        accountTransactions.forEach(function (transaction) {
+            list.push(renderTransactionsListCell(transaction))
+        })
+    }
+
+    return (
+        <List>{list}</List>
+    );
+}
+
+const renderTransactionsListCell = function(transaction) {
+    return (
+        <ListItem key={transaction.id}>
+            <div className="accountTransactionsListItemDiv">
+                <div className="accountTransactionsListItemDescription"><b>{transaction.description}</b></div>
+                <div className="accountTransactionsListItemDate">{transaction.date}</div>
+                <div className="accountTransactionsListItemOther">{checkTransactionType(transaction)}</div>
+            </div>
+        </ListItem>
+    );
+}
+
+const checkTransactionType = function(transaction) {
+    if (transaction.expense === true) {
+        return (
+            <div>{transactionTypeExpense(transaction)}</div>
+        );
+    } else {
+        return (
+            <div>{transactionTypeIncome(transaction)}</div>
+        );
+    }
+}
+
+const transactionTypeExpense = function(transaction) {
+    return (
+        <div className="accountTransactionsExpense">{transaction.amount}</div>
+    );
+}
+
+const transactionTypeIncome = function(transaction) {
+    return (
+        <div className="accountTransactionsIncome">{transaction.amount}</div>
+    );
+}
+
+export default AccountTransactions;
